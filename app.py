7a025947678df48050e875d914bf2a597dc19a6b
dendudko <84952870+dendudko@users.noreply.github.com>
2025-06-05 03:36:28 +1000

    Создана модель бд, нужна отладка, пока sqlite; стили и русификация; вырезана ролевая модель, нужно думать че делать с беспилотниками

--- app.py
+++ app.py
@@ -1,33 +1,25 @@
-from functools import wraps
+import os
+# from functools import wraps
 
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import jsonify
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
+from DB.model import db, User
 from Main.main import call_clustering, load_clustering_params, call_find_path, load_graph_params
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'FeAF<j,f322AfHnE_VfCnB#'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'DB', 'TheWay.db')
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-ROLES = ('operator', 'drone')
-
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(16), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+# ROLES = ('operator', 'drone')
 
 
 @login_manager.user_loader
@@ -35,22 +27,22 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-def role_required(role):
-    def decorator(f):
-        @wraps(f)
-        @login_required
-        def decorated_function(*args, **kwargs):
-            if current_user.role != role:
-                return "Access denied", 403
-            return f(*args, **kwargs)
+# def role_required(role):
+#     def decorator(f):
+#         @wraps(f)
+#         @login_required
+#         def decorated_function(*args, **kwargs):
+#             if current_user.role != role:
+#                 return "Access denied", 403
+#             return f(*args, **kwargs)
+#
+#         return decorated_function
+#
+#     return decorator
 
-        return decorated_function
 
-    return decorator
-
-
-operator_required = role_required('operator')
-drone_required = role_required('drone')
+# operator_required = role_required('operator')
+# drone_required = role_required('drone')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -62,7 +54,7 @@ def login():
         if user and user.check_password(request.form['password']):
             login_user(user)
             return redirect(url_for('index'))
-        flash("Invalid username or password")
+        flash("Неверное имя пользователя или пароль")
     return render_template('login.html')
 
 
@@ -73,19 +65,20 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        role = request.form['role']
-        if role not in ROLES:
-            flash("Invalid role")
-            return redirect(url_for('register'))
+        # role = request.form['role']
+        # if role not in ROLES:
+        #     flash("Invalid role")
+        #     return redirect(url_for('register'))
         if User.query.filter_by(username=username).first():
-            flash("Username already exists")
+            flash("Пользователь с таким именем уже существует")
             return redirect(url_for('register'))
-        user = User(username=username, role=role)
+        user = User(username=username)
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
-        flash("Registered successfully. Please log in.")
-        return redirect(url_for('login'))
+        login_user(user)
+        # flash("Registered successfully please log in.")
+        # return redirect(url_for('login'))
     return render_template('register.html')
 
 
@@ -96,22 +89,16 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/operator-zone')
-@operator_required
-def operator_zone():
-    return f"Welcome to operator zone, {current_user.username}"
-
-
-@app.route('/drone-zone')
-@drone_required
-def drone_zone():
-    return f"Welcome to drone zone, {current_user.username}"
-
-
-# Инициализация БД
-@app.before_request
-def create_tables():
-    db.create_all()
+# @app.route('/operator-zone')
+# @operator_required
+# def operator_zone():
+#     return f"Welcome to operator zone, {current_user.username}"
+#
+#
+# @app.route('/drone-zone')
+# @drone_required
+# def drone_zone():
+#     return f"Welcome to drone zone, {current_user.username}"
 
 
 @app.route('/post_graphs_parameters', methods=['POST'])
