0ecd0237c4798f7cb6b83100b782145349f76cc6
dendudko <84952870+dendudko@users.noreply.github.com>
2025-06-09 17:44:30 +1000

    Добавил минимальную проверку качества данных по входным файлам

--- LoadData/load_data.py
+++ LoadData/load_data.py
@@ -54,80 +54,92 @@ def store_dataset(dataset: pd.DataFrame, dataset_name, user_id, hash_value):
 
 
 def process_and_store_dataset(df_data, df_marine, dataset_name, user_id, interpolation, max_gap_minutes: int = 30):
-    df_data = read_csv_or_xlsx(df_data)
-    df_marine = read_csv_or_xlsx(df_marine)
-    if max_gap_minutes:
-        max_gap_minutes = int(max_gap_minutes)
+    try:
+        df_data = read_csv_or_xlsx(df_data)
+        df_marine = read_csv_or_xlsx(df_marine)
+        if not {'id_marine', 'lat', 'lon', 'speed', 'course', 'date_add', 'age'}.issubset(set(df_data.columns.tolist())):
+            raise Exception('проверяйте формат файла с данными о движении.')
+        if not {'id_marine', 'port', 'length'}.issubset(set(df_marine.columns.tolist())):
+            raise Exception('проверяйте формат файла с данными о судах.')
 
-    hash_value = hashlib.md5(
-        (df_data.to_csv() + df_marine.to_csv() + str(interpolation) + str(max_gap_minutes)).encode('utf-8')).hexdigest()
+        if max_gap_minutes:
+            max_gap_minutes = int(max_gap_minutes)
 
-    result_integrity_check = integrity_check(hash_value, dataset_name)
-    if result_integrity_check is not None:
-        return result_integrity_check
+        hash_value = hashlib.md5(
+            (df_data.to_csv() + df_marine.to_csv() + str(interpolation) + str(max_gap_minutes)).encode(
+                'utf-8')).hexdigest()
 
-    df_data['timestamp'] = pd.to_datetime(df_data['date_add']) - pd.to_timedelta(df_data['age'], unit='m')
-    df_data = pd.merge(df_data, df_marine[['id_marine', 'port', 'length']], how='left', on='id_marine').dropna(axis=0)
-    df_data = df_data.loc[(df_data['course'] != 511) & (df_data['port'] != 0) & (df_data['length'] != 0)].reset_index(
-        drop=True)
-    df_data = df_data[['id_marine', 'lat', 'lon', 'speed', 'course', 'timestamp']]
-    df_data = (
-        df_data
-        .drop_duplicates(subset=['id_marine', 'lat', 'lon', 'speed', 'course'], keep='first')
-        .drop_duplicates(subset=['id_marine', 'timestamp'], keep='first')
-        .dropna(axis=0)
-    )
-    df_data = df_data.sort_values(['id_marine', 'timestamp'])
+        result_integrity_check = integrity_check(hash_value, dataset_name)
+        if result_integrity_check is not None:
+            return result_integrity_check
 
-    if interpolation:
-        result = []
-        for ship_id, group in df_data.groupby('id_marine'):
-            full_time = pd.date_range(group['timestamp'].min(), group['timestamp'].max(), freq='1min')
-            group = group.set_index('timestamp').reindex(full_time)
-            group['id_marine'] = ship_id
-            result.append(group)
-        df_data = pd.concat(result)
-        df_data.index.name = 'timestamp'
+        df_data['timestamp'] = pd.to_datetime(df_data['date_add']) - pd.to_timedelta(df_data['age'], unit='m')
+        df_data = pd.merge(df_data, df_marine[['id_marine', 'port', 'length']], how='left', on='id_marine').dropna(
+            axis=0)
+        df_data = df_data.loc[
+            (df_data['course'] != 511) & (df_data['port'] != 0) & (df_data['length'] != 0)].reset_index(
+            drop=True)
+        df_data = df_data[['id_marine', 'lat', 'lon', 'speed', 'course', 'timestamp']]
+        df_data = (
+            df_data
+            .drop_duplicates(subset=['id_marine', 'lat', 'lon', 'speed', 'course'], keep='first')
+            .drop_duplicates(subset=['id_marine', 'timestamp'], keep='first')
+            .dropna(axis=0)
+        )
         df_data = df_data.sort_values(['id_marine', 'timestamp'])
 
-        def interpolate_with_gap(g):
-            orig = g[g[['lat', 'lon', 'speed', 'course']].notna().all(axis=1)]
-            time_diff = orig.index.to_series().diff().dt.total_seconds() / 60
-            group_id = (time_diff > max_gap_minutes).cumsum().reindex(g.index, method='ffill').fillna(0).astype(int)
-            g['gap_group'] = group_id.values
+        if interpolation:
+            result = []
+            for ship_id, group in df_data.groupby('id_marine'):
+                full_time = pd.date_range(group['timestamp'].min(), group['timestamp'].max(), freq='1min')
+                group = group.set_index('timestamp').reindex(full_time)
+                group['id_marine'] = ship_id
+                result.append(group)
+            df_data = pd.concat(result)
+            df_data.index.name = 'timestamp'
+            df_data = df_data.sort_values(['id_marine', 'timestamp'])
 
-            for col in ['lat', 'lon', 'speed']:
+            def interpolate_with_gap(g):
+                orig = g[g[['lat', 'lon', 'speed', 'course']].notna().all(axis=1)]
+                time_diff = orig.index.to_series().diff().dt.total_seconds() / 60
+                group_id = (time_diff > max_gap_minutes).cumsum().reindex(g.index, method='ffill').fillna(0).astype(int)
+                g['gap_group'] = group_id.values
+
+                for col in ['lat', 'lon', 'speed']:
+                    for grp, sub_g in g.groupby('gap_group'):
+                        mask = sub_g[[col]].notna().any(axis=1)
+                        if mask.sum() >= 2:
+                            g.loc[sub_g.index, col] = sub_g[col].interpolate(method='time')
+
+                # КОРРЕКТНАЯ интерполяция курса:
                 for grp, sub_g in g.groupby('gap_group'):
-                    mask = sub_g[[col]].notna().any(axis=1)
+                    mask = sub_g[['course']].notna().any(axis=1)
                     if mask.sum() >= 2:
-                        g.loc[sub_g.index, col] = sub_g[col].interpolate(method='time')
+                        # Переводим курс в синус и косинус
+                        sin_course = np.sin(np.deg2rad(sub_g['course']))
+                        cos_course = np.cos(np.deg2rad(sub_g['course']))
+                        # Интерполируем синус и косинус
+                        sin_course_interp = sin_course.interpolate(method='time')
+                        cos_course_interp = cos_course.interpolate(method='time')
+                        # Восстанавливаем курс
+                        course_interp = np.rad2deg(np.arctan2(sin_course_interp, cos_course_interp))
+                        course_interp = (course_interp + 360) % 360
+                        g.loc[sub_g.index, 'course'] = course_interp
 
-            # КОРРЕКТНАЯ интерполяция курса:
-            for grp, sub_g in g.groupby('gap_group'):
-                mask = sub_g[['course']].notna().any(axis=1)
-                if mask.sum() >= 2:
-                    # Переводим курс в синус и косинус
-                    sin_course = np.sin(np.deg2rad(sub_g['course']))
-                    cos_course = np.cos(np.deg2rad(sub_g['course']))
-                    # Интерполируем синус и косинус
-                    sin_course_interp = sin_course.interpolate(method='time')
-                    cos_course_interp = cos_course.interpolate(method='time')
-                    # Восстанавливаем курс
-                    course_interp = np.rad2deg(np.arctan2(sin_course_interp, cos_course_interp))
-                    course_interp = (course_interp + 360) % 360
-                    g.loc[sub_g.index, 'course'] = course_interp
+                g = g.drop(['gap_group'], axis=1)
+                return g
 
-            g = g.drop(['gap_group'], axis=1)
-            return g
+            df_data = df_data.groupby('id_marine').apply(interpolate_with_gap)
 
-        df_data = df_data.groupby('id_marine').apply(interpolate_with_gap)
+        df_data = df_data[['lat', 'lon', 'speed', 'course']].dropna(axis=0).drop_duplicates()
+        df_data = df_data.rename(columns={'lat': 'latitude', 'lon': 'longitude'})
 
-    df_data = df_data[['lat', 'lon', 'speed', 'course']].dropna(axis=0).drop_duplicates()
-    df_data = df_data.rename(columns={'lat': 'latitude', 'lon': 'longitude'})
+        store_dataset(df_data, dataset_name, user_id, hash_value)
 
-    store_dataset(df_data, dataset_name, user_id, hash_value)
+        return True, f'Создан датасет: {dataset_name}'
 
-    return True, f'Создан датасет: {dataset_name}'
+    except Exception as exc:
+        return False, f'Ошибка при создании датасета: {exc}'
 
 
 def load_positions_cleaned(hash_id):
